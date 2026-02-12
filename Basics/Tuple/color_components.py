df7b20b49217e8c5d397903d419113c33d292bbf
NandhanaMangalathMenon <nandhanamangalathmenon@gmail.com>
2026-02-12 22:09:33 +0530

    Create color_components.py

---
+++ Basics/Tuple/color_components.py
@@ -0,0 +1,12 @@
+# Store RGB values as a tuple
+color = (125, 200, 255)
+
+# Access and print each component of the tuple
+print("Red component:", color[0])    # First value in the tuple
+print("Green component:", color[1])  # Second value in the tuple
+print("Blue component:", color[2])   # Third value in the tuple
+
+#RGB color value represents a color using a combination of red,green,blue
+#RGB color values range from 0 to 255
+#0 for white
+#255 for black
