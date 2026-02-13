b20b7391207d1586112efc80134d2a1087f13be0
NandhanaMangalathMenon <nandhanamangalathmenon@gmail.com>
2026-02-13 20:39:47 +0530

    Create max_numbers.py

---
+++ Basics/Functions/max_numbers.py
@@ -0,0 +1,9 @@
+#Write a Python function to return the maximum of three numbers.
+
+def max_of_three(a, b, c):
+    if a >= b and a >= c:
+        return a
+    elif b >= a and b >= c:
+        return b
+    else:
+        return c
